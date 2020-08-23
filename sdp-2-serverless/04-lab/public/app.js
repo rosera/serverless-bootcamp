10b9417104eac0e21b13e531ade7945eab848517
Rich Rose <askrichardrose@gmail.com>
2020-08-24 00:03:43 +0100

    Initial version - 04-lab tested

---
+++ sdp-2-serverless/04-lab/public/app.js
@@ -0,0 +1,93 @@
+// Add some JavaScript variables
+const filename = "data/language2.json"; 
+const btnForm  = document.querySelector('#btn');
+
+let jsonData;
+let jsonTranslate;
+const URL_ENDPOINT="https://us-central1-roselabs-212512.cloudfunctions.net/translate-service";
+
+// Read the contents of the json file
+fetchAsyncLocalData();
+
+// Load the information from the data directory
+async function fetchAsyncLocalData() {
+  // Await the fetch execution
+  const data = await(fetch(filename));
+  // Await the data being available
+  jsonData = await data.json();
+
+  console.log(jsonData);
+//  return jsonInfo;
+}
+
+// Perform the update of the HTML element
+async function getDataElement(url, htmlElement){
+  htmlElement.textContent = 'Processing';
+
+  // Get the information URL
+  fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    htmlElement.textContent = data;
+  })
+  .catch((error) => {
+    console.error('Fetch error:', error);
+  });
+}
+
+// Create a new object
+async function getTranslation(items, text){
+  const langTranslation = items.map((item) => {
+    let url = URL_ENDPOINT + '?msg=' + text + '&lang=' + item.translate;
+    console.log('URL Endpoint:' + url);
+
+    //let url = URL_ENDPOINT + '?msg=' + text;
+    htmlElement = document.querySelector(`#${item.translate}`);
+    getDataElement(url, htmlElement);
+  });
+}
+
+// Call for a language translation
+function getLanguageTranslation(text){
+  getTranslation(jsonData.Translations, text);
+}
+
+// Update the onscreen view
+function setViewContent(items){
+  const dynamicView = items.map((item) => {
+    return `
+    <div class="grid-item">
+      <h2>${item.language}</h2>
+      <img src=${item.flag} alt="${item.language}">
+      <h3 id="${item.translate}">${item.language}</h3>
+     </div>`;
+  });
+
+  // Return a string - without a comma  
+  return dynamicView.join("");  
+}
+
+
+// Build onscreen information
+function getLanguageView() {
+  console.log('buildDataDisplay(): Called');
+  languageGrid = document.querySelector('#grid');
+  languageGrid.innerHTML = setViewContent(jsonData.Translations);
+}
+
+
+// Link to form button event
+btnForm.addEventListener('click', (event) => {
+  // Get the message entered on the form
+  const inputMsg = document.querySelector('#message').value;
+
+  // Display the grid
+  getLanguageView();
+
+  // Get Translation
+  getLanguageTranslation(inputMsg);
+});
+  
+
+
